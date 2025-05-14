358773b7d94868d8721ef51b424c6c1eaeb60f01
RexTrue <114847416+RexTrue@users.noreply.github.com>
2025-05-14 15:08:23 +0700

    Add files via upload

---
+++ MVC_/src/Model/Connector.java
@@ -0,0 +1,32 @@
+package Model;
+import java.sql.*;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class Connector {
+    private static String jdbc_driver = "com.mysql.cj.jdbc.Driver";
+    private static String nama_db = "upnvybaru";
+    private static String url_db = "jdbc:mysql://localhost:3306/" + nama_db;
+    
+    private static String username_db = "root";
+    private static String password_db = "";
+    
+    static Connection conn;
+    
+    public static Connection Connect() {
+        try {
+        Class.forName(jdbc_driver);
+        conn = DriverManager.getConnection(url_db, username_db, password_db);           
+        }catch (ClassNotFoundException | SQLException exception) {
+            System.out.println(exception.getLocalizedMessage());
+        }
+        return conn;
+    }
+}
